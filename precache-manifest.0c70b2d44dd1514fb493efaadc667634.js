fae792221a568ffdcd0304ee2d3c3556c22fc6e4
Nicholas Lim <nicholas.yt88@gmail.com>
2020-07-19 22:54:53 +0800

    Updates

---
+++ precache-manifest.0c70b2d44dd1514fb493efaadc667634.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92625f5267d0680eaa47922eb11d2ef",
+    "url": "/dr-app/index.html"
+  },
+  {
+    "revision": "3c67ab84d2c7c030ca25",
+    "url": "/dr-app/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "77c14b01d808c3f5804d",
+    "url": "/dr-app/static/css/main.7aaa092f.chunk.css"
+  },
+  {
+    "revision": "3c67ab84d2c7c030ca25",
+    "url": "/dr-app/static/js/2.599f6d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dr-app/static/js/2.599f6d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c14b01d808c3f5804d",
+    "url": "/dr-app/static/js/main.4348756a.chunk.js"
+  },
+  {
+    "revision": "0179fe148c8a2cae9ab9",
+    "url": "/dr-app/static/js/runtime-main.8ed75b73.js"
+  },
+  {
+    "revision": "136f9cd0959c3d8826aceda089bb02ee",
+    "url": "/dr-app/static/media/Lalezar-Regular.136f9cd0.ttf"
+  },
+  {
+    "revision": "9e3f68db51ca03cc9c9cf5c5ca30908b",
+    "url": "/dr-app/static/media/tigerbalm.9e3f68db.svg"
+  }
+]);
