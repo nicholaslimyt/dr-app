7ff768352d6c6c1e9f0a97d47fb5a3d551814acb
Nicholas Lim <nicholas.yt88@gmail.com>
2020-07-19 22:49:38 +0800

    test

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo/tigerbalm.svg';
+import 'antd/dist/antd.css'
+//import { Button, DatePicker } from 'antd';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work in Progress . . .
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_self"
+        >
+          Darkness Reveal
+        </a>
+
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
