732088cc1ef6f4140a5f17d51772b91f651d8df9
Nicholas Lim <nicholas.yt88@gmail.com>
2020-07-19 22:00:30 +0800

    add

---
+++ precache-manifest.aab91717f90e9497f799fe6d78eb2ae8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109b15b7bd5a68fd07f6ecdc4302d812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c67ab84d2c7c030ca25",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "1926430c942b5001dc60",
+    "url": "/static/css/main.e2f78611.chunk.css"
+  },
+  {
+    "revision": "3c67ab84d2c7c030ca25",
+    "url": "/static/js/2.599f6d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.599f6d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1926430c942b5001dc60",
+    "url": "/static/js/main.9f185008.chunk.js"
+  },
+  {
+    "revision": "e59efe12e26afa1e78bc",
+    "url": "/static/js/runtime-main.fee9ae46.js"
+  },
+  {
+    "revision": "136f9cd0959c3d8826aceda089bb02ee",
+    "url": "/static/media/Lalezar-Regular.136f9cd0.ttf"
+  },
+  {
+    "revision": "9e3f68db51ca03cc9c9cf5c5ca30908b",
+    "url": "/static/media/tigerbalm.9e3f68db.svg"
+  }
+]);
